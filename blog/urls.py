8b010769fff4298134bfe872354639e43320ec04
Your Name <pprasad@C02SG771G8WP.local>
2018-04-09 13:06:53 -0400

    Heroku config files and updates

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.Blog_List_View.as_view(), name='home'),
     path('post/<int:pk>/', views.Blog_Detail_View.as_view(), name='post_detail'),
+    path('post/new/', views.Blog_Create_View.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', views.Blog_Update_View.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', views.Blog_Delete_View.as_view(), name='post_delete'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 class Blog_List_View(ListView):
@@ -9,3 +11,21 @@ class Blog_List_View(ListView):
 class Blog_Detail_View(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class Blog_Create_View(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+
+class Blog_Update_View(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+    template_name = 'post_edit.html'
+
+
+class Blog_Delete_View(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
